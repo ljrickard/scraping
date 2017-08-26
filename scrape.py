bdcedd92003a3b317935e8bb25440345be7848ab
Luke Rickard <luke.x.rickard@gmail.com>
2017-08-26 10:39:32 +0100

    scraping kiehls site map

--- scrape.py
+++ scrape.py
@@ -1,51 +1,33 @@
 #!/usr/bin/env python3
-
+import logging
 import requests
 import json
 from bs4 import BeautifulSoup as bs
-request = requests.get("http://www.kiehls.co.uk/skin-care/category/view-all-skincare")
-print(request.status_code)
-soup = bs(request.text,  "html.parser") 
-#print(soup)
-#some_elements = soup.find_all("a", class_="product_name")
+from http import HTTPStatus
+import sys
 
-result_list = soup.find_all("div", class_="search_result_items")
-#print(len(result_list))
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+logger = logging.getLogger(__name__)
 
-all_products = soup.find_all("div", class_="product_tile_wrapper b-product_tile-wrapper ")
-#print(len(all_products))
-#print(all_products)
+BASE_URL = "http://www.kiehls.co.uk"
+SITE_MAP = "/site-map.html"
 
-product_ids = soup.find_all("div", class_="b-custom-product_tile-wrapper js-product-details")
-#print(len(product_ids))
-
-def has_class_but_no_id(tag):
-    return tag.has_attr('data-product-url')
-
-product_urls = soup.find_all(has_class_but_no_id)
-
-for product_url in product_urls:
-	print(product_url['data-product-url'])
-#data-product-url
-
-#for product in product_ids:
-#	print(json.loads(product['data-trackinginfo'])['productID'])
-#	print(json.loads(product['data-trackinginfo'])['productName'])
-
-# exclude duplicate products / collections
-
-#{'itemtype':'http://schema.org/GeoCoordinates'}
-# some_elements = soup.find_all("a", class_="product_name")
-#from urllib.request import urlopen
-#page = urlopen("http://www.kiehls.co.uk/skin-care/category/view-all-skincare")
-#soup = bs(page, "html.parser") 
-
-#itemtype="http://schema.org/Product"
-#some_elements = soup.find_all('div', {'itemtype':'http://schema.org/Product'})
-#print(len(some_elements))
-#for element in some_elements:
-#	print(element)
+def get_hrefs(soup, keyword):
+	return [a.get('href') for a in soup.find_all('a') if keyword in a.get('href')]
 
 
-#some_elements = soup.find_all("div", itemtype="http://schema.org/Product")
-#print(len(some_elements))
+logging.info("Scraping site %s", BASE_URL)
+
+site_map_response = requests.get(BASE_URL + SITE_MAP)
+
+#logging.info("http status_code=%s recieved from %s", site_map_response.status_code, BASE_URL + SITE_MAP)
+
+soup = bs(site_map_response.text,  "html.parser")
+
+skin_care_hrefs = get_hrefs(soup, 'skin-care') # create regex containing skin care
+
+print(skin_care_hrefs)
+
+
+
+
