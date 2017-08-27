3db5e99c52e678d1b70b37aa2be88733f365b9b4
Luke Rickard <luke.x.rickard@gmail.com>
2017-08-27 11:39:05 +0100

    description extracted

--- scrape.py
+++ scrape.py
@@ -36,12 +36,56 @@ def get_product_urls(skin_care_hrefs):
 def has_class_but_no_id(tag):
     return tag.has_attr('data-trackinginfo')
 
-#data_trackinginfo = soup.find_all(has_class_but_no_id)[-1].extract().data_trackinginfo.get('data-trackinginfo')
-#ingredients_copy = soup.find_all('div', id='ing-copy')
-
-
 #soup = make_soup("http://www.kiehls.co.uk/skin-care/category/serums/hydro-plumping-re-texturizing-serum-concentrate/255.html")
 soup = make_soup("http://www.kiehls.co.uk/skin-care/category/facial-masks/ultra-facial-overnight-hydrating-masque/3601.html")
+
+# first part locates the tags and the second extracts that particular attribute data
+data_trackinginfo = soup.find_all(has_class_but_no_id)[-1].extract().get('data-trackinginfo')
+ingredients_copy = soup.find_all('div', id='ing-copy')
+
+#for copy in ingredients_copy:
+#	print(copy.get_text())
+
+product_details = soup.find_all('div', class_='l-product_details-wrapper')[-1].extract()
+product_names = product_details.find_all('span', class_='product_name')
+
+# for product_name in product_names:
+# 	print(product_name.get_text())
+
+product_subtitle_raw = product_details.find_all('h2', class_='product_subtitle')
+
+# for product_subtitle in product_subtitle_raw:
+# 	print(product_subtitle.get_text())
+
+
+def remove_hrefs(tag):
+	return not tag.a and not tag.has_attr('href')
+
+product_details_description = product_details.\
+									find_all('div', class_='product_detail_description')[-1].extract().\
+									find_all(remove_hrefs)
+
+product_description = ''
+
+for copy in product_details_description:	
+	copy = copy.get_text().strip().strip('\n').replace('\n', '.')
+
+	while '  ' in copy:
+		copy = copy.replace('  ', ' ')
+
+	while '..' in copy:
+		copy = copy.replace('..', '.')
+
+	# print(copy.get_text().split('\n'))
+	# copy = copy.get_text().replace('\n', '')
+	# copy = copy.get_text().rstrip("\n")
+	if copy not in product_description:
+		product_description+=copy
+
+print(product_description)
+
+# for description in product_description.find_all(remove_hrefs):
+# 	print(description.get_text())
 
 
 #product_details = soup.find_all('div', class_='l-product_details-wrapper')[-1].extract()
