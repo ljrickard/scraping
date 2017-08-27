024a743280d5e3a14748de738167779fe844aecc
Luke Rickard <luke.x.rickard@gmail.com>
2017-08-27 09:07:03 +0100

    tracking info + ingredients copy

--- scrape.py
+++ scrape.py
@@ -4,7 +4,7 @@ import requests
 import json
 from bs4 import BeautifulSoup as bs
 from http import HTTPStatus
-import sys
+import sys, html5lib
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 logger = logging.getLogger(__name__)
@@ -13,7 +13,7 @@ BASE_URL = "http://www.kiehls.co.uk"
 SITE_MAP = "/site-map.html"
 
 def make_soup(url):
-	return bs(requests.get(url).text,  "html.parser")
+	return bs(requests.get(url).text,  "html5lib")
 
 def get_hrefs(soup, keyword):
 	return [a.get('href') for a in soup.find_all('a') if keyword in a.get('href')]
@@ -36,27 +36,15 @@ def get_product_urls(skin_care_hrefs):
 def has_class_but_no_id(tag):
     return tag.has_attr('data-trackinginfo')
 
+#data_trackinginfo = soup.find_all(has_class_but_no_id)[-1].extract().data_trackinginfo.get('data-trackinginfo')
+#ingredients_copy = soup.find_all('div', id='ing-copy')
+
+
 #soup = make_soup("http://www.kiehls.co.uk/skin-care/category/serums/hydro-plumping-re-texturizing-serum-concentrate/255.html")
 soup = make_soup("http://www.kiehls.co.uk/skin-care/category/facial-masks/ultra-facial-overnight-hydrating-masque/3601.html")
-product_details = soup.find_all('div', class_='l-product_details-wrapper')[-1].extract()
 
 
-#data_trackinginfo = product_details.find_all(has_class_but_no_id)[-1].extract()
-#data_trackinginfo2 = data_trackinginfo.get('data-trackinginfo')
-
-
-ingredients_raw = soup.find_all('div', {'class':['ingredient ', 'ingredient ingredient-hidden']}) #[-1].extract() #class="ingredient "
-
-ingredients = dict()
-
-for ingredient in ingredients_raw:
-	if ingredient.div and ingredient.div.h3 and ingredient.div.p:
-		#ingredients.add(tuple({ingredient.div.h3.get_text() : ingredient.div.p.get_text()}.items()))
-		ingredients[ingredient.div.h3.get_text()] = ingredient.div.p.get_text()
-
-print(len(ingredients))
-
-
+#product_details = soup.find_all('div', class_='l-product_details-wrapper')[-1].extract()
 
 
 
