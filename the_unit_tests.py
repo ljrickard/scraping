b81547f2229b617834207f48e7f1212926fcc703
Luke Rickard <luke.x.rickard@gmail.com>
2017-10-10 21:23:58 +0100

    adding unit tests

---
+++ the_unit_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import unittest
+
+
+class MyTest(unittest.TestCase):
+    def test_something(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+	unittest.main()	
