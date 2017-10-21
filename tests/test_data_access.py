e86a9b360828b97fa68e69696e12bb5733b0cd03
Luke Rickard <luke.x.rickard@gmail.com>
2017-10-21 14:19:09 +0100

    fixing circle ci failure

--- tests/test_data_access.py
+++ tests/test_data_access.py
@@ -8,7 +8,8 @@ class DataAccessTests(unittest.TestCase):
     def setUp(self):
         self.data_access = DataAccess()
 
-    def test_data_access_should_only_write_to_local_file_when_dry_run_is_true(self):
+    def test_data_access_should_only_write_to_local_file_when_dry_run_is_true(
+            self):
         self.assertTrue(True)
 
 
