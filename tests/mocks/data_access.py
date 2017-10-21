38848243a18a16f52567c91abb4157f8df46f3df
Luke Rickard <luke.x.rickard@gmail.com>
2017-10-21 17:13:56 +0100

    building out unit tests 2

--- tests/mocks/data_access.py
+++ tests/mocks/data_access.py
@@ -2,4 +2,20 @@
 class MockedDataAccess():
 
     def __init__(self, dry_run=False):
-        pass
+        self.existing_products = []
+        self.save_products_invoked = False
+        self.filter_existing_products_invoked = False
+        self.get_product_template_invoked = False
+
+    def get_product_template(self):
+        self.get_product_template_invoked = True
+
+    def filter_existing_products(self, arg1, arg2):
+        self.filter_existing_products = True
+        return self.existing_products
+
+    def set_existing_products(self, existing_products):
+        self.existing_products = existing_products
+
+    def save_products(self):
+        self.save_products_invoked = True
--- tests/test_data_access.py
+++ tests/test_data_access.py
@@ -8,10 +8,6 @@ class DataAccessTests(unittest.TestCase):
     def setUp(self):
         self.data_access = DataAccess()
 
-    def test_data_access_should_only_write_to_local_file_when_dry_run_is_true(
-            self):
-        self.assertTrue(True)
-
 
 if __name__ == '__main__':
     unittest.main()
